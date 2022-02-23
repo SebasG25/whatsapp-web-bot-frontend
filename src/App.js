e609ce2081e90b0ab31d2d7ae8746582658917a5
Sebastian Guzman <bitas200225@gmail.com>
2022-02-23 02:12:32 -0500

    Send message and media by url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './styles/App.module.css';
+import axios from 'axios'
 
 function App() {
+
+  const [message, setMessage] = useState({ message: '', to: '', urlImage: '' })
+
+  const sendMessage = () => {
+    axios.post('http://localhost:9000/send', { message: message.message, to: message.to })
+      .then(() => console.log('Message sent succesfully'))
+      .catch(err => console.error(err))
+  }
+
+  const sendMedia = () => {
+    axios.post('http://localhost:9000/sendMedia',
+      {
+        message: message.message,
+        to: message.to,
+        urlImage: message.urlImage
+      })
+      .then(() => console.log('Media sent succesfully'))
+      .catch(err => console.error(err))
+  }
+
+  const handleChange = (e) => {
+    setMessage({ ...message, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <input
+          className={styles.button}
+          name='to'
+          placeholder='573024639509'
+          type="text"
+          onChange={handleChange}
+        />
+        <textarea
+          className={`${styles.message} ${styles.button}`}
+          name='message'
+          placeholder='Hey there!'
+          type="text"
+          onChange={handleChange}
+        />
+        <input
+          className={styles.button}
+          type="text"
+          name="urlImage"
+          placeholder='URL image'
+          onChange={handleChange}
+        />
+        <button className={styles.sendButton} onClick={message.urlImage === '' ? sendMessage : sendMedia}>
+          Send message
+        </button>
       </header>
     </div>
   );
